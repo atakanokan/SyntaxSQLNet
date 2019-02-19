3993ca4f899e6588a13bbaf167593a8d227df916
atakanokan <atakanokan@outlook.com>
2019-02-19 09:11:21 -0500

    added active training page

--- flaskexample/views.py
+++ flaskexample/views.py
@@ -1,3 +1,7 @@
+"""
+Start Flask app by running ./run_flask.py
+"""
+
 from flaskexample import app
 from flask import request
 from flask import render_template
@@ -57,5 +61,33 @@ def cesareans_output():
                          question = eng_q,
                          database_name = db_name_q,
                           generated_sql = gen_sql,
+                          tables = tables_html,
+                          titles = titles)
+
+@app.route('/retrain')
+def retrain_flask():
+
+   # pull english question and tokenize it
+   # eng_q = "What are the maximum and minimum budget of the departments?"
+   eng_q = request.args.get('english_question')
+   print("Question: {}".format(eng_q))
+
+   # pull the database name for the question
+   # db_name_q = 'department_management'
+   db_name_q = request.args.get('database_name')
+   print("Database Name: {}".format(db_name_q))
+
+   # pull the correct query from the website of output
+   correct_query = request.args.get('correct_query')
+   print("Correct Query: {}".format(correct_query))
+
+   # get tables from database
+   tables_html, titles = get_tables_html(db_name = db_name_q)
+   print("Number of Tables in DB: {}".format(len(tables_html)))
+
+   return render_template("retrain.html", 
+                         question = eng_q,
+                         database_name = db_name_q,
+                          correct_query = correct_query,
                           tables = tables_html,
                           titles = titles)
